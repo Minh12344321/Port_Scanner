cb1c4e2871c00722d3bcf7db72b8a92bedd9161a
Minh12344321 <2251272729@e.tlu.edu.vn>
2024-10-30 16:02:14 +0700

    Add list port

---
+++ Port_Client/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Port_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(218, 48);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(379, 191);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(534, 351);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send :>>";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Port_Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Địa chỉ IP và cổng server
+        IPAddress ipAddress = IPAddress.Any;
+        int port = 12345;
+
+        // Tạo TcpListener để lắng nghe kết nối từ client
+        TcpListener server = new TcpListener(ipAddress, port);
+        server.Start();
+        Console.WriteLine($"Server đang chạy tại IP {ipAddress} trên cổng {port}...");
+
+        // Chấp nhận kết nối từ client
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client đã kết nối!");
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string clientMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Nhận tin nhắn từ client: {clientMessage}");
+
+            // Kiểm tra nếu client gửi tin nhắn "port", thì mới bắt đầu quét cổng
+            if (clientMessage.ToLower() == "port")
+            {
+                Console.WriteLine("Bắt đầu quét cổng theo yêu cầu từ client...");
+
+                // Gọi hàm quét các cổng đang mở
+                string openPorts = ListOpenPorts();
+
+                // Gửi kết quả quét về cho client
+                byte[] responseData = Encoding.UTF8.GetBytes(openPorts);
+                stream.Write(responseData, 0, responseData.Length);
+            }
+            else
+            {
+                // Phản hồi lại client với thông điệp khác
+                string responseMessage = "Server đã nhận tin nhắn của bạn!";
+                byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+                stream.Write(responseData, 0, responseData.Length);
+            }
+
+            client.Close();
+        }
+    }
+
+    // Hàm liệt kê các cổng đang mở trên server
+    static string ListOpenPorts()
+    {
+        StringBuilder openPorts = new StringBuilder();
+        for (int port = 1; port <= 65535; port++)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                listener.Stop();
+            }
+            catch (SocketException)
+            {
+                // Chỉ thêm thông tin cổng mở vào danh sách khi có exception (cổng đang mở)
+                openPorts.AppendLine($"Port {port} đang mở.");
+            }
+        }
+        return openPorts.ToString();
+    }
+}
